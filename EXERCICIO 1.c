01f002c51ee9d7b34690c43ee0a36ac29ba5f182
S l a p y <115092557+slapyng@users.noreply.github.com>
2022-10-05 11:50:33 -0300

    Add files via upload
    um programa que leia um número inteiro e depois imprima a mensagem
    “Valor lido:”, seguido do valor inteiro.

---
+++ EXERCICIO 1.c
@@ -0,0 +1,16 @@
+int main()
+{
+    int v1, v2, v3 ,v4 ,v5, v6;
+    v1 = 25;
+    v2 = 15;
+    v3 = 34;
+    v4 = 64;
+    v5 = 85;
+    printf("\nVetor1: %d", v1);
+    printf("\nVetor2: %d", v2);
+    printf("\nVetor3: %d", v3);
+    printf("\nVetor4: %d", v4);
+    printf("\nVetor5: %d", v5);
+    v6 = v1 + v2 + v3 + v4 + v5;
+    printf("\nO resultado final é: %d", v6);
+}
