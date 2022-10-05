ea09e7df72f4a873b652bacd51ca40197fba1a00
S l a p y <115092557+slapyng@users.noreply.github.com>
2022-10-05 11:56:38 -0300

    Add files via upload
    um programa que solicite ao usuário entrar com o valor do dia, mês e
    ano (inteiros). Em seguida, imprima os valores lidos separados por uma barra (/).

---
+++ EXERCICIO 9.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+float main() {
+  int dia, mes, ano;
+  printf("Escreva dia de entrada: ");
+  scanf("%d", &dia);
+   printf("Mes: ");
+    scanf("%d", &mes);
+    printf("Ano: ");
+  scanf("%d", &ano);
+    printf("Ä data de entrada do usuário ao sistema foi de: %d/%d/%d", dia, mes, ano);
+
+  return 0;
+    
+  
+}
+  
