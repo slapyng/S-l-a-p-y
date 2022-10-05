2d9f4a516eb53d6cc3bb018e3695b5f0dfe3905c
S l a p y <115092557+slapyng@users.noreply.github.com>
2022-10-05 11:48:18 -0300

    Add files via upload

---
+++ SOMA de 5 numeros.c
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+ int num,num1,num2,num3,num4,num5;
+  num = 25;
+  num1 = 15;
+  num2 = 39;
+  num3 = 65;
+  num4 = 85;
+   num5 = num + num1 + num2 + num3 + num4;
+  printf("SOMA:%d",num5);
+
+    return 0;
+}
