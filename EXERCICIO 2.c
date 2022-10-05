1ccaafcc962e8d4bdb2960069f846ba6bf519eb1
S l a p y <115092557+slapyng@users.noreply.github.com>
2022-10-05 11:53:15 -0300

    Add files via upload
    um programa que leia um número inteiro e depois o imprima.

---
+++ EXERCICIO 2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+int v1 = 25;
+int v2 = 15;
+int v3 = 39;
+int v4 = 64;
+int v5 = 85;
+int v6;
+    v6 = v1 + v2 + v3 + v4 + v5;
+    printf("\nVetor1: %d", v1);
+    printf("\nVetor2: %d", v2);
+    printf("\nVetor3: %d", v3);
+    printf("\nVetor4: %d", v4);
+    printf("\nVetor5: %d", v5);
+    printf("\nO resultado  de todos os vetores: %d", v6);
+}
